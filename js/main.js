a1515fa6bce2ef51da2880eb7a8a3ae4e13cc9aa
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-29 15:24:41 -0300

    Adicionado validações no calculo do imc com operadores lógicos

--- js/main.js
+++ js/main.js
@@ -14,4 +14,20 @@ var nome = tdNome.textContent;
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 var imcCalculado = peso / (altura * 2);
-var imc = tdImc.textContent = imcCalculado;
+
+var ehPesoValido = true;
+var ehAlturaValida = true;
+
+if (peso < 0 || peso > 1000) {
+    alert(`O peso: ${peso} é inválido!`);
+    ehPesoValido = false;
+}
+
+if (altura < 1.00 || altura > 3.00) {
+    alert(`A altura: ${altura} é inválida!`);
+    ehAlturaValida = false;
+}
+
+if (ehPesoValido && ehAlturaValida) {
+    var imc = tdImc.textContent = imcCalculado;
+}
