ffc39653ad096b4245b4e154b668ce049add2fd5
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-29 14:59:15 -0300

    Adicionando calculo do imc para o primeiro paciente.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 var titulo = document.querySelector('.titulo');
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdNome = paciente.querySelector('.info-nome');
+var tdPeso = paciente.querySelector('.info-peso');
+var tdAltura = paciente.querySelector('.info-altura');
+var tdImc = paciente.querySelector('.info-imc');
+
+
+var nome = tdNome.textContent;
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+var imcCalculado = peso / (altura * 2);
+var imc = tdImc.textContent = imcCalculado;
