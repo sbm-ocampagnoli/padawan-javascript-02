fc585481e17e76a48942988877ec914161ad9b96
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-31 15:26:36 -0300

    Adicionando função de IMC.

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -36,8 +36,15 @@ for (let i = 0; i < pacientes.length; i++) {
     }
 
     if (ehPesoValido && ehAlturaValida) {
-        imc = tdImc.textContent = imcCalculado.toFixed(2);
+        tdImc.textContent = calcularIMC(peso, altura);
     }
+}
+
+function calcularIMC(peso, altura) {
+    let imc;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+
 }
 
 function destacarPacienteInvalido(paciente) {
--- js/form.js
+++ js/form.js
@@ -15,17 +15,19 @@ adicionarPacienteBtn.addEventListener("click", function (event) {
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
 
     nomeTd.textContent = nome;
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
-
+    imcTd.textContent = calcularIMC(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
