a78d75d16472ba69b064a03e217f5a6f0ac40f24
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-05-31 10:49:12 -0300

    Adicionando pacientes e mudando style da linha nas validações.

--- js/main.js
+++ js/main.js
@@ -2,33 +2,45 @@ var titulo = document.querySelector('.titulo');
 
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdNome = paciente.querySelector('.info-nome');
-var tdPeso = paciente.querySelector('.info-peso');
-var tdAltura = paciente.querySelector('.info-altura');
-var tdImc = paciente.querySelector('.info-imc');
+for (let i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdNome = paciente.querySelector('.info-nome');
+    var tdPeso = paciente.querySelector('.info-peso');
+    var tdAltura = paciente.querySelector('.info-altura');
+    var tdImc = paciente.querySelector('.info-imc');
 
 
-var nome = tdNome.textContent;
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-var imcCalculado = peso / (altura * 2);
-var imc;
+    var nome = tdNome.textContent;
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var imcCalculado = peso / (altura * 2);
+    var imc;
 
-var ehPesoValido = true;
-var ehAlturaValida = true;
+    var ehPesoValido = true;
+    var ehAlturaValida = true;
 
-if (peso <= 0 || peso >= 1000) {
-    imc = tdImc.textContent = `O peso: ${peso} é inválido!`;
-    ehPesoValido = false;
+    if (peso <= 0 || peso >= 1000) {
+        imc = tdImc.textContent = `O peso: ${peso} é inválido!`;
+        ehPesoValido = false;
+        marcarPacienteInvalido(paciente);
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        imc = tdImc.textContent = `A altura: ${altura} é inválida!`;
+        ehAlturaValida = false;
+        marcarPacienteInvalido(paciente);
+    }
+
+    if (ehPesoValido && ehAlturaValida) {
+        imc = tdImc.textContent = imcCalculado.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 3.00) {
-    imc = tdImc.textContent = `A altura: ${altura} é inválida!`;
-    ehAlturaValida = false;
-}
-
-if (ehPesoValido && ehAlturaValida) {
-    imc = tdImc.textContent = imcCalculado;
+function marcarPacienteInvalido(paciente) {
+    paciente.style.backgroundColor = 'lightCoral';
+    paciente.style.color = 'white';
 }
