2b3c07a2c3cd306deace017ca79fc1a13b4215da
sbm-ocampagnoli <otaviocampagnoli@hotmail.com>
2024-06-03 15:40:04 -0300

    Adicionando função de remover paciente.

---
+++ js/remover-paciente.js
@@ -0,0 +1,11 @@
+var tabela = pegarTabelaDePacientes();
+
+
+tabela.addEventListener('dblclick', function (event) {
+    let elementoAlvo = event.target;
+    let paciente = elementoAlvo.parentNode;
+
+    if (paciente.tagName == 'TR'); {
+        paciente.remove();
+    }
+});
